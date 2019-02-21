0bc4f88695dab1516e2c2c1c84cb03b33be8f766
Alexander <vorobyovvorobyov@gmail.com>
2019-02-21 23:51:27 +0300

    Add tests

---
+++ Assets/UGF.Assemblies.Runtime.Tests/TestAssemblyUtility.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace UGF.Assemblies.Runtime.Tests
+{
+    public class TestAssemblyUtility
+    {
+        [Test]
+        public void GetBrowsableTypesGeneric()
+        {
+            int count = AssemblyUtility.GetBrowsableTypes<AssemblyBrowsableTypeAttribute>().Count();
+            
+            Assert.AreEqual(1, count);
+        }
+
+        [Test]
+        public void GetBrowsableTypes()
+        {
+            int count = AssemblyUtility.GetBrowsableTypes(typeof(AssemblyBrowsableTypeAttribute)).Count();
+            
+            Assert.AreEqual(1, count);
+        }
+
+        [Test]
+        public void GetBrowsableAssemblies()
+        {
+            int count = AssemblyUtility.GetBrowsableAssemblies().Count();
+            
+            Assert.AreEqual(1, count);
+        }
+    }
+}
