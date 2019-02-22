4a1b607b2633aef74223cef813ec0cb850e89bbe
Alexander <vorobyovvorobyov@gmail.com>
2019-02-22 23:07:30 +0300

    Resolves #3

--- Packages/UGF.Assemblies/Runtime/AssemblyBrowsableTypeAttribute.cs
+++ Packages/UGF.Assemblies/Runtime/AssemblyBrowsableTypeAttribute.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace UGF.Assemblies.Runtime
 {
+    /// <summary>
+    /// Attribute used to mark specific type as available for search.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | AttributeTargets.Delegate | AttributeTargets.Enum)]
     public class AssemblyBrowsableTypeAttribute : Attribute
     {
