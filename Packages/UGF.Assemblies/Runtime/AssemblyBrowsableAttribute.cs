d1aef32cb5997eb0380a3167fb150416dd042244
Vorobey <vorobyovvorobyov@gmail.com>
2019-02-22 23:10:09 +0300

    Revert "Develop 1"

--- Packages/UGF.Assemblies/Runtime/AssemblyBrowsableAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace UGF.Assemblies.Runtime
-{
-    [AttributeUsage(AttributeTargets.Assembly)]
-    public sealed class AssemblyBrowsableAttribute : Attribute
-    {
-    }
-}
